5e664c8f060c64f6211204db3427b9f56f7e951c
Brandon Little <brlittle86@gmail.com>
2017-01-11 13:42:45 -0800

    stuff

--- about.js
+++ about.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('Hellow from about.js!');
+var formEl = document.getElementById('first-form');
+
+formEl.addEventListener('submit', function(event){
+  event.preventDefault();
+  event.stopPropagation();
+
+  renderUsername(event.target.username.value);
+
+}, false);
+
+function renderUsername(username) {
+  var sectionEl = document.getElementById('form-section');
+
+  var userNameEl = document.createElement('p');
+  userNameEl.setAttribute('class', 'entered-username');
+  userNameEl.textContent = username;
+
+  sectionEl.appendChild(userNameEl);
+}
